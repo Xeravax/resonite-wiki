56d4e34a60ac49254ae5b07b6c66f5b4e90ef1b5
Richard Fox <richard.fox@ahref.co.uk>
2026-01-07 20:40:04 -0800

    automatically sync backups to R2

---
+++ scripts/syncBackups.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "[$(date)] Starting R2 sync"
+
+rclone sync /database-backups r2:${R2_BUCKET_NAME}/database-backups \
+  --transfers 4 \
+  --checkers 8 \
+  --log-level INFO
+
+rclone sync /files-backups r2:${R2_BUCKET_NAME}/files-backups \
+  --transfers 4 \
+  --checkers 8 \
+  --log-level INFO
+
+echo "[$(date)] R2 sync completed"
