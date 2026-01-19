5e2357dbe1a64cfb15a29581c494c6a88136d460
Richard Fox <richard.fox@ahref.co.uk>
2026-01-19 00:29:08 -0800

    relocate most meta-data to theme for now

--- config/Themes.php
+++ config/Themes.php
@@ -15,4 +15,22 @@ $wgDefaultSkin = "citizen";
 // Default skin to dark mode
 $wgCitizenThemeDefault = 'dark';
 
+$wgCitizenEnableCommandPalette = true;
+$wgCitizenHeaderPosition = 'left';
+
+$wgSitename = "Resonite Wiki";
+$wgMetaNamespace = "Resonite_Wiki";
+$wgUsePrivateIPs = true;
+
+$prefix = $finalCDNURL;
+
+## The URL paths to the logo.  Make sure you change this from the default,
+## or else you'll overwrite your logo when you upgrade!
+$wgLogos = [
+	'1x' => "$prefix/8/8f/Resonite_Wiki-Logo1x.png",
+	'icon' => "$prefix/1/11/Resonite_Wiki-Icon.png",
+];
+
+
+
 ?>
